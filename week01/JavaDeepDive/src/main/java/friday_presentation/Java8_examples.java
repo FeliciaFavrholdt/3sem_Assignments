edb79cdeb3329a61d2d72eeb7faa3f79f5d21762
FilleFilejs <cph-ff62@cphbusiness.dk>
2024-01-31 20:59:08 +0100

    adds solution for task1 - Lambda

---
+++ week01/JavaDeepDive/src/main/java/friday_presentation/Java8_examples.java
@@ -0,0 +1,9 @@
+package friday_presentation;
+
+public class Java8_examples {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ week01/JavaDeepDive/src/main/java/taskOne_Lambda/ArithmeticOperation.java
@@ -0,0 +1,6 @@
+package taskOne_Lambda;
+
+@FunctionalInterface
+public interface ArithmeticOperation {
+    int perform(int a, int b);
+}
